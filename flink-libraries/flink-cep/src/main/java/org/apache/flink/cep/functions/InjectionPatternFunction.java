2471140e5ce7e6afad682202259c8192f16ae8a0
zhangpu-paul <zhangpu_pau@126.com>
2020-11-04 15:03:35 +0800

    flink-cep 动态规则

--- flink-libraries/flink-cep/src/main/java/org/apache/flink/cep/functions/InjectionPatternFunction.java
+++ flink-libraries/flink-cep/src/main/java/org/apache/flink/cep/functions/InjectionPatternFunction.java
@@ -5,14 +5,32 @@ import org.apache.flink.cep.pattern.Pattern;
 
 import java.io.Serializable;
 
-
+/**
+ *
+ * @param <T>
+ */
 public interface InjectionPatternFunction<T> extends Function, Serializable {
 
+	/**
+	 * 你可能有一些初始化的工作
+	 */
 	public void init();
 
+	/**
+	 * 获取新的pattern
+	 * @return
+	 */
 	public Pattern<T,T> inject();
 
+	/**
+	 * 一个扫描周期:ms
+	 * @return
+	 */
 	public long getPeriod();
 
+	/**
+	 * 规则是否发生变更
+	 * @return
+	 */
 	public boolean isChanged();
 }
